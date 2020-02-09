54c68501f0e4d15764ecaa40a5ee82da57da88fc
imalgrab <igormalgrab@gmail.com>
2020-02-09 13:54:36 +0100

    deleted server

--- app.js
+++ app.js
@@ -35,4 +35,10 @@ app.use((req, res, next) => {
     res.status(404).render('404');
 });
 
+app.set('port', 2115);
+
+const server = app.listen(app.get('port'), () => {
+    console.log(`App listening on ${server.address().port}`);
+});
+
 module.exports = app;
--- server.js
+++
@@ -1,7 +0,0 @@
-const app = require('./app');
-
-app.set('port', 2115);
-
-const server = app.listen(app.get('port'), () => {
-    console.log(`App listening on ${server.address().port}`);
-});
